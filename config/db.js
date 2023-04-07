cddc4e365b0c3ee0a73210aafcdb440bd6f973c3
atheist31 <rkb080@gmail.com>
2023-04-07 22:30:25 +0530

    backend

--- config/db.js
+++ config/db.js
@@ -3,6 +3,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const connection = mongoose.connect(
-  "mongodb+srv://ravi:bhashkar@cluster0.gr1lnxe.mongodb.net/bewkoof?retryWrites=true&w=majority"
+ process.env.mongoURL
 );
 module.exports = { connection };
